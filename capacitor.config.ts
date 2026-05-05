c17eef16004a95f1e08cfd48e7eae13d834ab363
istikbal türüt <istikbalturut@gmail.com>
2026-05-04 21:38:05 -0400

    feat: add Capacitor iOS platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iturut.voicenoteapp',
+  appName: 'iTurut',
+  webDir: 'build'
+};
+
+export default config;
