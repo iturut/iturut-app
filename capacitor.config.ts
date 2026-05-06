f12d122b03e70f4c3a0c9e9a41483cc90c258b03
istikbal türüt <istikbalturut@gmail.com>
2026-05-06 09:33:24 -0400

    fix: add iOS server config for Firebase auth

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
   appId: 'com.iturut.voicenoteapp',
   appName: 'iTurut',
-  webDir: 'build'
+  webDir: 'build',
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'https',
+    hostname: 'localhost'
+  }
 };
-
 export default config;
